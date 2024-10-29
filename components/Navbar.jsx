1548070459a44e9ba2ab4dda8794ca385f86e05f
dilmohan <macbookpro@MacBooks-MacBook-Pro.local>
2024-10-29 08:02:06 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className={styles.nav}>
+        <p>Next.js</p>
+        <ul c>
+
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Navbar;
